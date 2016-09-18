f4f00eff754060cbd418491d52ae35d9acc22b8b
Maarten Brouwers <github.com@murb.nl>
2016-09-18 20:42:09 +0200

    moving to rails engine.

---
+++ lib/social_linker/engine.rb
@@ -0,0 +1,4 @@
+module SocialLinker
+  class Engine < ::Rails::Engine
+  end
+end
--- lib/social_linker/railtie.rb
+++ lib/social_linker/railtie.rb
@@ -1,9 +1,8 @@
-module SocialLinker
-  class Railtie < Rails::Railtie
-    ActiveSupport.on_load(:action_view) do
-      puts "Action view loaded, including SocialLinker::ViewHelpers"
-      require 'social_linker/view_helpers'
-      include SocialLinker::ViewHelpers
-    end
-  end
-end
+# module SocialLinker
+#   class Railtie < Rails::Railtie
+#     ActiveSupport.on_load(:action_view) do
+#       require 'social_linker/view_helpers'
+#       include SocialLinker::ViewHelpers
+#     end
+#   end
+# end
